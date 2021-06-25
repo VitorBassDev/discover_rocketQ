cd590038652cb73c09e61fb1ba1910ba81c3e3e4
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-25 14:02:31 -0300

    Refactor: Configurarar pagina incial do projeto com o EJS!

--- back/src/server.js
+++ back/src/server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const route   = require('./route')
+const path    = require('path')
 require('dotenv/config')
 
 const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static("public"))
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(route)
 
 app.listen(process.env.BACKEND_PORT, () => {
   console.log(`Executando servidor na porta`, process.env.BACKEND_PORT)
