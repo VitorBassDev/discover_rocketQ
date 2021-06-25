dd401f838aeef034a78c0bb3e1c43b45deac0d48
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-25 16:37:13 -0300

    Feat: Configuraçao de novas rotas Parametros das rotas para Marcar a pergunta como lida, e para excluir a pergunta

--- back/src/router.js
+++ back/src/router.js
@@ -3,5 +3,11 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+// route.get('/create-pass', (req,res) => res.render("index", {page: 'create-pass'}))
+// route.get('/room', (req,res) => res.render("index", {page: 'room'}))
+
+route.get('/create-pass', (req,res) => res.render("create-pass"))
+route.get('/room', (req,res) => res.render("room"))
+route.get('/room2', (req,res) => res.render("room2"))
 
 module.exports = route
