ae3311e670b1906e7dc3eb457f327160ca387497
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-26 08:10:24 -0300

    Refactor: Ajustar rotas da aplicaçao Ajustar URL para redirecionar para a pagina de criar sala Ajustart rotas no arquivo rotas

--- back/src/router.js
+++ back/src/router.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+// const RoomController = require('./controllers/RoomController')
+
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
-// route.get('/create-pass', (req,res) => res.render("index", {page: 'create-pass'}))
-// route.get('/room', (req,res) => res.render("index", {page: 'room'}))
-
+route.get('/', (req, res) => res.render("index"))
 route.get('/create-pass', (req,res) => res.render("create-pass"))
 route.get('/room', (req,res) => res.render("room"))
-route.get('/room2', (req,res) => res.render("room2"))
+
+route.post('/question/:room/:question/:action', QuestionController.index)
+// route.post('/create-room', RoomController.create)
 
 module.exports = route
