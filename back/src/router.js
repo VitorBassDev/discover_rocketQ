c9d9e0b85b6109f6f92df35bd82e2b6c4a3dfb14
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-26 08:41:04 -0300

    Refactor: Ajuste de rotas no arquivo router.js

--- back/src/router.js
+++ back/src/router.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
-// const RoomController = require('./controllers/RoomController')
+const RoomController = require('./controllers/RoomController')
 
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
 route.get('/create-pass', (req,res) => res.render("create-pass"))
-route.get('/room', (req,res) => res.render("room"))
+route.get('/room/:room', (req, res) => res.render("room"))
 
 route.post('/question/:room/:question/:action', QuestionController.index)
-// route.post('/create-room', RoomController.create)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
