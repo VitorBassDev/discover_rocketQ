a95a7dac088a13d52565f9229382d56e2572cf7d
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-28 18:45:55 -0300

    Feat: Novas Rotas

--- back/src/router.js
+++ back/src/router.js
@@ -9,7 +9,11 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/create-pass', (req,res) => res.render("create-pass"))
 route.get('/room/:room', (req, res) => res.render("room"))
 
-route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
